de0ac174b14e58d81635f9f8ab5d3ff13fce5112
Jay <jerry915077@gmail.com>
2021-04-09 16:53:31 +0800

    chore: style search bar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,66 +1,58 @@
 <template>
-   <v-card
-    color="red lighten-2"
-    dark
-  >
-    <v-card-title class="headline red lighten-3">
-      Search for Public APIs
-    </v-card-title>
-    <v-card-text>
-      Explore hundreds of free API's ready for consumption! For more information visit
-      <a
-        class="grey--text text--lighten-3"
-        href="https://github.com/toddmotto/public-apis"
-        target="_blank"
-      >the Github repository</a>.
-    </v-card-text>
-    <v-card-text>
-      <v-autocomplete
-        v-model="model"
-        :items="items"
-        :loading="isLoading"
-        :search-input.sync="search"
-        color="white"
-        hide-no-data
-        hide-selected
-        item-text="Description"
-        item-value="API"
-        label="Public APIs"
-        placeholder="Start typing to Search"
-        prepend-icon="mdi-database-search"
-        return-object
-      ></v-autocomplete>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-expand-transition>
-      <v-list
-        v-if="model"
-        class="red lighten-3"
-      >
-        <v-list-item
-          v-for="(field, i) in fields"
-          :key="i"
-        >
-          <v-list-item-content>
-            <v-list-item-title v-text="field.value"></v-list-item-title>
-            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-expand-transition>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn
-        :disabled="!model"
-        color="grey darken-3"
-        @click="model = null"
-      >
-        Clear
-        <v-icon right>
-          mdi-close-circle
-        </v-icon>
-      </v-btn>
-    </v-card-actions>
+  <v-card color="red lighten-2" dark>
+    <div class="navBar">
+      <v-card-title class="headline red lighten-3">
+        搜尋公司名稱
+      </v-card-title>
+      <div class="searchBar">
+        <v-card-text>
+          <v-autocomplete
+            v-model="model"
+            :items="items"
+            :loading="isLoading"
+            :search-input.sync="search"
+            color="white"
+            hide-no-data
+            hide-selected
+            item-text="Description"
+            item-value="API"
+            label="Public APIs"
+            placeholder="Start typing to Search"
+            prepend-icon="mdi-database-search"
+            return-object
+          >
+          </v-autocomplete>
+        </v-card-text>
+        <v-expand-transition>
+          <v-list
+            v-if="model"
+            class="red lighten-3"
+          >
+            <v-list-item
+              v-for="(field, i) in fields"
+              :key="i"
+            >
+              <v-list-item-content>
+                <v-list-item-title v-text="field.value"></v-list-item-title>
+                <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-expand-transition>
+      </div>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          :disabled="!model"
+          color="grey darken-3"
+          @click="model = null"
+        >Clear
+          <v-icon right>
+            mdi-close-circle
+          </v-icon>
+        </v-btn>
+      </v-card-actions>
+    </div>
   </v-card>
 </template>
 
@@ -120,3 +112,12 @@ export default {
     },
 };
 </script>
+<style scoped>
+.navBar{
+  display: flex;
+  justify-content: space-between;
+}
+.searchBar{
+  width: 80%;
+}
+</style>
