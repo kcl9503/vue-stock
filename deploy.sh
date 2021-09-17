69a49d968e19fd8e0e06b64731f72dea70288610
Jay <jerry915077@gmail.com>
2021-09-17 11:44:14 +0800

    docs: modify config

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,25 @@
-//deploy.sh
+#!/usr/bin/env sh
+
+# abort on errors
 set -e
 
+# build
 npm run build
 
+# navigate into the build output directory
 cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
 git add -A
 git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 git push -f https://github.com/kcl9503/vue-stock.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
 cd -
